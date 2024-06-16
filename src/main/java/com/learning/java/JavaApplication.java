af1b327f10f9a295d6ce058fac951c89b055357a
yesienna <sunvihowell@wp.pl>
2024-06-16 16:47:20 +0200

    Created the first Bean

--- src/main/java/com/learning/java/JavaApplication.java
+++ src/main/java/com/learning/java/JavaApplication.java
@@ -2,16 +2,21 @@ package com.learning.java;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class JavaApplication {
 
 	public static void main(String[] args) {
 
-        SpringApplication.run(JavaApplication.class, args);
+        var ctx = SpringApplication.run(JavaApplication.class, args);
 
-        MyFirstClass myFirstClass = new MyFirstClass();
+        MyFirstClass myFirstClass = ctx.getBean(MyFirstClass.class);
         System.out.println(myFirstClass.sayHello());
 	}
+    @Bean
+    public MyFirstClass myFirstClass() {
+        return new MyFirstClass();
+    }
 
 }
